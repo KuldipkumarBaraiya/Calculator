159cdcc2ebd0978bbcf0f859b00de2aeabc4e62a
Kuldip Baraiya <kuldipbaraiya7096@gmail.com>
2022-12-02 11:25:31 +0530

    Divition File Modify

--- division.java
+++ division.java
@@ -1,5 +1,7 @@
 public class division {
     public static void main(String[] args) {
-        
+        int a = 10 , b = 20;
+        int ans = b/a;
+        System.out.println("Dividion : "+ans);
     }
 }
