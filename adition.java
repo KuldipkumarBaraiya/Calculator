02d59a4d730d434d9c1d9f0e6e49c049d19dc51d
Kuldip Baraiya <kuldipbaraiya7096@gmail.com>
2022-12-02 11:05:54 +0530

    Modify Adition File

--- adition.java
+++ adition.java
@@ -1,5 +1,7 @@
 public class adition {
     public static void main(String[] args) {
-        
+        int a =10,b=20;
+        int ans = a+b;
+        System.out.println("Adition : "+ans);
     }
 }
